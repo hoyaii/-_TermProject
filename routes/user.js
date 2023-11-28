e5bd5e0eb4ba75e8e554abb1f345f3ac1221ebfd
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-11-28 21:25:31 +0900

    feat: sequelize 도입

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,57 @@
 const express = require('express');
 const { isLoggedIn } = require('./middlewares');
-const User = require('../models/user');
+const { User, Order } = require('../models');
 
 const router = express.Router();
 
-router.get('/', isLoggedIn, async (req, res, next) => {
+// 사용자 정보 조회
+router.get('/:userId', isLoggedIn, async (req, res, next) => {
     try {
-        const user = await User.find({ where: { id: req.user.id } });
+        const user = await User.findOne({
+            where: { id: req.params.userId },
+        });
         if (!user) {
-        return res.status(404).send('No user found.');
-    }
-    res.json(user);
+            return res.status(404).send('User not found');
+        }
+        res.json(user);
     } catch (error) {
         console.error(error);
-        return next(error);
+        next(error);
+    }
+});
+
+// 사용자 정보 수정
+router.put('/:userId', isLoggedIn, async (req, res, next) => {
+    try {
+        const updatedUser = await User.update({
+            email: req.body.email,
+            name: req.body.name,
+        }, {
+            where: { id: req.params.userId },
+        });
+        if (!updatedUser) {
+            return res.status(404).send('User not found');
+        }
+        res.json(updatedUser);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+// 사용자 계정 삭제
+router.delete('/:userId', isLoggedIn, async (req, res, next) => {
+    try {
+        const result = await User.destroy({
+            where: { id: req.params.userId },
+        });
+        if (!result) {
+            return res.status(404).send('User not found');
+        }
+        res.status(204).send();
+    } catch (error) {
+        console.error(error);
+        next(error);
     }
 });
 
