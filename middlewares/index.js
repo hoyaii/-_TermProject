d32fc5f66ddd684e6bb2dccd40fffc8018b6710d
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-11-29 15:56:35 +0900

    feat: login.html 구현

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,17 +1,17 @@
 exports.isLoggedIn = (req, res, next) => {
-    if (req.isAuthenticated()) { // req.isAuthenticated()는 passport에서 제공하는 함수로, 사용자가 로그인 상태인지 확인합니다.
-        next(); // 로그인 상태라면 다음 미들웨어로 넘어갑니다.
+    if (req.isAuthenticated()) { // req.isAuthenticated()는 passport에서 제공하는 함수로, 사용자가 로그인 상태인지 확인함.
+        next(); // 로그인 상태라면 다음 미들웨어로 넘어간다.
     } else {
-        res.status(403).send('로그인 필요'); // 로그인 상태가 아니라면 403 에러를 반환합니다.
+        res.status(403).send('로그인 필요'); // 로그인 상태가 아니라면 403 에러를 반환
     }
 };
 
 exports.isNotLoggedIn = (req, res, next) => {
-    if (!req.isAuthenticated()) { // req.isAuthenticated()는 passport에서 제공하는 함수로, 사용자가 로그인 상태인지 확인합니다.
-        next(); // 로그인 상태가 아니라면 다음 미들웨어로 넘어갑니다.
+    if (!req.isAuthenticated()) {
+        next(); // 로그인 상태가 아니라면 다음 미들웨어로 넘어감.
     } else {
         const error = new Error('로그인한 상태입니다.');
         error.status = 403;
-        next(error); // 로그인 상태라면 403 에러를 반환합니다.
+        next(error); // 로그인 상태라면 403 에러를 반환
     }
 };
