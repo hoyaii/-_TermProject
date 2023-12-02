dcfaf7c29a4c47fdf74258aa95cd8032a27fd13f
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-12-02 13:08:13 +0900

    refactor: cutomer_write_review에서 가짜 데이터를 사용하지 않고 쿼리 함수를 사용 하도록 수정

--- controller/reviews.js
+++ controller/reviews.js
@@ -1,16 +1,18 @@
 const { Review } = require('../models');
 
 exports.createReview = async (req, res, next) => {
+    const { orderId, rating, comment } = req.body;
+    const userId = req.session.userId; // 세션에서 사용자 ID를 가져옵니다.
+
+    const sql = "INSERT INTO Review (order_id, customer_id, rating, comment) VALUES (?, ?, ?, ?)";
+
     try {
-        const newReview = await Review.create({
-            userId: req.user.id,
-            restaurantId: req.body.restaurantId,
-            content: req.body.content,
-            rating: req.body.rating,
-        });
-        res.json(newReview);
+        await connection.query(sql, [orderId, userId, rating, comment]);
+
+        res.status(200).send('리뷰가 성공적으로 작성되었습니다.');
     } catch (error) {
-        console.error(error);
+        console.error('Error:', error);
+        res.status(500).send('리뷰 작성에 실패하였습니다.');
         next(error);
     }
 };
