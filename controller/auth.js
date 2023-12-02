a901acd37e4db460cdfbffb5906bced377c29e39
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-12-02 21:37:09 +0900

    fix: auth.js에서 로그아웃시 리다이렉트 경로 수정

--- controller/auth.js
+++ controller/auth.js
@@ -39,6 +39,6 @@ exports.login = (req, res, next) => {
 
 exports.logout = (req, res) => {
     req.logout(() => {
-        res.redirect('/auth/login');
+        res.redirect('/login.html');
     });
 };
