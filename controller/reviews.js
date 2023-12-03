861262536d8e8481e9f970e0eef69c9cb6c25502
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-12-04 02:12:12 +0900

    feat: 식당 주인이 리뷰를 확인할 때 사용하는 getReview() 함수 구현

--- controller/reviews.js
+++ controller/reviews.js
@@ -17,3 +17,32 @@ exports.createReview = async (req, res, next) => {
         next(error);
     }
 };
+
+exports.getReview = async (req, res, next) => {
+    const restaurantId = req.params.restaurantId; // URL 경로에서 restaurantId를 가져옵니다.
+
+    try {
+        // restaurant_id에 해당하는 주문의 order_id와 menu_id를 가져옵니다.
+        let [orderRows] = await db.query("SELECT order_id, menu_id FROM Orders WHERE restaurant_id = ?", [restaurantId]);
+
+        let reviewHistory = [];
+
+        // 각 주문에 대한 정보를 추가합니다.
+        for(let order of orderRows) {
+            let [menuRows] = await db.query("SELECT name FROM Menu WHERE menu_id = ?", [order.menu_id]);
+            let [reviewRows] = await db.query("SELECT comment FROM Review WHERE order_id = ?", [order.order_id]);
+
+            reviewHistory.push({
+                orderId: order.order_id,
+                menuName: menuRows[0].name,
+                comment: reviewRows[0].comment,
+            });
+        }
+
+        res.status(200).json(reviewHistory);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('리뷰 조회에 실패하였습니다.');
+        next(error);
+    }
+};
--- routes/reviews.js
+++ routes/reviews.js
@@ -7,4 +7,7 @@ const router = express.Router();
 // 리뷰 작성
 router.post('/', isLoggedIn, createReview);
 
+// 리뷰 확인 by 식당 주인
+router.get('restaurants/${restaurantId}', isLoggedIn, getReview)
+
 module.exports = router;
